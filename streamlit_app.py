f72a6e3e4f438660a25947313660019a19d0ed47
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-03-25 01:31:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import google.generativeai as genai
 
-st.title('🎈 App Name')
+genai.configure(api_key="AIzaSyDKcxALky8LiROaxb0RGMw8TLLOcujMRMY")
+llm = genai.GenerativeModel(model_name="gemini-pro")
 
-st.write('Hello world!')
+def prompt(text):
+    prompt_parts = [
+        f'"Given a text: {text} analyze the text and recommend a book that the user will like reading in their current situation/mood. the output should contain the book name and a line explaining why that book was chosen"'
+    ]
+    return prompt_parts
+def generate_book(text):
+    human_prompt = prompt(text)
+    response = llm.generate_content(human_prompt)
+    return response.text
+def main():
+    st.title("Book Recommendation based on Text Analysis")
+    user_input = st.text_input("Enter your text:")
+
+    if st.button("Generate Book Recommendation"):
+        recommendation = generate_book(user_input)
+        st.markdown(recommendation)
+
+if __name__ == "__main__":
+    main()
